82dbbb25a5fa9f9dc410ebddf1e74e78bc2bbee3
ashbinthapa <ashbinthapa2054@gmail.com>
2024-02-23 14:50:17 +0545

    start to make header and footer file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/header', function () {
+    return view('layouts/header');
 });
+
+Route::get('/', function () {
+    return view('home');
+});
+
